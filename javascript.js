7ee93911373e82060ec2991ce940cda7da2488ad
eliseweir <67879336+eliseweir@users.noreply.github.com>
2020-10-19 18:05:12 -0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// site counter
+let visitors = localStorage.getItem('on_load_counter');
+
+if (visitors === null) {
+    visitors = 0;
+}
+visitors++;
+
+localStorage.setItem('on_load_counter', visitors);
+document.getElementById('counter').innerHTML = visitors.toString();
+
+// Buy Now button
+let buyNowButton = document.querySelector('#buy-button');
+
+function onButtonClick() {
+    buyNowButton.innerHTML = "Buy Again";
+    window.open('https://www.sprite.com/products/', '_blank');
+};
+
+buyNowButton.addEventListener('click', onButtonClick);
